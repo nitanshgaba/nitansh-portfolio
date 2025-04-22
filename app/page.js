31b1a42c2e2032b8c1dbf43facade18f5ac0bf95
nitansh gaba <112342600+nitanshgaba@users.noreply.github.com>
2025-04-22 19:59:26 +0530

    Added 'use client' to resolve document is not defined issue

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client'; // Add this line at the top
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
@@ -26,7 +28,7 @@ export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
